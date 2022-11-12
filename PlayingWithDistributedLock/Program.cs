6ea86750e68b9782f6e1e6b41abeb93ef25c93ca
Balazs Kereszturi <19balazs86@gmail.com>
2022-11-12 15:33:18 +0100

    Replace Parallel.For to Parallel.ForEachAsync

--- PlayingWithDistributedLock/Program.cs
+++ PlayingWithDistributedLock/Program.cs
@@ -2,85 +2,85 @@
 
 namespace PlayingWithDistributedLock
 {
-  public class Program
-  {
-    private static readonly Random _random = new Random();
-
-    private const string _lockKey = "lock:eat";
-
-    // Note: Read the description in the ExceptionToleranceLockFactory class.
-    private static readonly ILockFactory _lockFactory =
-      new ExceptionToleranceLockFactory(new RedisDistLockFactory());
-
-    private static readonly Policy<ILockObject> _waitAndRetryPolicy = Policy
-      .HandleResult<ILockObject>(lo => lo.IsAcquired == false) // When we did not get a lock.
-      .WaitAndRetry(4, // 1 + 4 times retry.
-        _ => TimeSpan.FromMilliseconds(_random.Next(1200, 1500)),
-        (res, ts, ctx) => Console.WriteLine($"{ctx["person"]} is waiting for retry - {ts.TotalMilliseconds} ms."));
-
-    /// <summary>
-    /// Main
-    /// </summary>
-    public static async Task Main(string[] args)
+    public class Program
     {
-      // --> #1 AcquireLock with retry.
-      using (ILockObject outerLockObject = _lockFactory.AcquireLock(_lockKey, TimeSpan.FromSeconds(3)))
-      using (ILockObject innerLockObject = _lockFactory.AcquireLock(_lockKey, TimeSpan.FromSeconds(5), 2, TimeSpan.FromSeconds(2)))
-        Console.WriteLine($"Did I get a lock? -> {innerLockObject.IsAcquired}");
+        private static readonly Random _random = new Random();
 
-      // --> #2 AcquireLockAsync with retry.
-      using (ILockObject outerLockObject = await _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(3)))
-      using (ILockObject innerLockObject = await _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(5), 2, TimeSpan.FromSeconds(2)))
-        Console.WriteLine($"Did I get a lock? -> {innerLockObject.IsAcquired}");
+        private const string _lockKey = "lock:eat";
 
-      // --> #3 AcquireLockAsync with retry + timeout.
-      try
-      {
-        using (CancellationTokenSource ctSource = new CancellationTokenSource(TimeSpan.FromSeconds(3.5)))
+        // Note: Read the description in the ExceptionToleranceLockFactory class.
+        private static readonly ILockFactory _lockFactory =
+            new ExceptionToleranceLockFactory(new RedisDistLockFactory());
+
+        private static readonly AsyncPolicy<ILockObject> _waitAndRetryPolicy = Policy
+            .HandleResult<ILockObject>(lo => lo.IsAcquired == false) // When we did not get a lock.
+            .WaitAndRetryAsync(4, // 1 + 4 times retry.
+                _ => TimeSpan.FromMilliseconds(_random.Next(1200, 1500)),
+                (res, ts, ctx) => Console.WriteLine($"{ctx["person"]} is waiting for retry - {ts.TotalMilliseconds} ms."));
+
+        /// <summary>
+        /// Main
+        /// </summary>
+        public static async Task Main(string[] args)
         {
-          using (ILockObject outerLockObject = await _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(3)))
-          using (ILockObject innerLockObject = await _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(5), 2, TimeSpan.FromSeconds(2), ctSource.Token))
-            Console.WriteLine($"Did I get a lock? -> {innerLockObject.IsAcquired}");
+            // --> #1 AcquireLock with retry.
+            using (ILockObject outerLockObject = _lockFactory.AcquireLock(_lockKey, TimeSpan.FromSeconds(3)))
+            using (ILockObject innerLockObject = _lockFactory.AcquireLock(_lockKey, TimeSpan.FromSeconds(5), 2, TimeSpan.FromSeconds(2)))
+                Console.WriteLine($"Did I get a lock? -> {innerLockObject.IsAcquired}");
+
+            // --> #2 AcquireLockAsync with retry.
+            using (ILockObject outerLockObject = await _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(3)))
+            using (ILockObject innerLockObject = await _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(5), 2, TimeSpan.FromSeconds(2)))
+                Console.WriteLine($"Did I get a lock? -> {innerLockObject.IsAcquired}");
+
+            // --> #3 AcquireLockAsync with retry + timeout.
+            try
+            {
+                using (CancellationTokenSource ctSource = new CancellationTokenSource(TimeSpan.FromSeconds(3.5)))
+                {
+                    using (ILockObject outerLockObject = await _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(3)))
+                    using (ILockObject innerLockObject = await _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(5), 2, TimeSpan.FromSeconds(2), ctSource.Token))
+                        Console.WriteLine($"Did I get a lock? -> {innerLockObject.IsAcquired}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("I expected for an OperationCanceledException.");
+            }
+
+            // --> #4 Dinner is ready to eat.
+            await Parallel.ForEachAsync(Enumerable.Range(1, 4), (personId, ct) => personEat(personId));
+
+            Console.WriteLine("End of the dinner.");
         }
-      }
-      catch (OperationCanceledException)
-      {
-        Console.WriteLine("I expected for an OperationCanceledException.");
-      }
 
-      // --> #4 Dinner is ready to eat.
-      Parallel.For(0, 5, x => personEat(x));
-
-      Console.WriteLine("End of the dinner.");
-    }
-
-    private static void personEat(int personId)
-    {
-      string person = $"Person({personId})";
-
-      // Try to acquire a lock maximum 5 times.
-      ILockObject lockObject = _waitAndRetryPolicy
-        .Execute(
-          ctx => _lockFactory.AcquireLock(_lockKey, TimeSpan.FromSeconds(5)),
-          new Dictionary<string, object> { {"person", person} });
-
-      if (lockObject.IsAcquired)
-      {
-        // We got a lock.
-        Console.WriteLine($"{person} begin eat food.");
-
-        Thread.Sleep(1000);
-
-        // Try to release the lock.
-        if (_random.NextDouble() < 0.8)
+        private static async ValueTask personEat(int personId)
         {
-          lockObject.Release();
+            string person = $"Person({personId})";
 
-          Console.WriteLine($"{person} released the lock.");
+            // Try to acquire a lock maximum 5 times.
+            ILockObject lockObject = await _waitAndRetryPolicy
+                .ExecuteAsync(
+                    ctx => _lockFactory.AcquireLockAsync(_lockKey, TimeSpan.FromSeconds(5)),
+                    new Dictionary<string, object> { ["person"] = person } );
+
+            if (lockObject.IsAcquired)
+            {
+                // We got a lock.
+                Console.WriteLine($"{person} begin eat food.");
+
+                await Task.Delay(1_000);
+
+                // Try to release the lock.
+                if (_random.NextDouble() < 0.8)
+                {
+                    await lockObject.ReleaseAsync();
+
+                    Console.WriteLine($"{person} released the lock.");
+                }
+                else Console.WriteLine($"{person} did not release lock.");
+            }
+            else Console.WriteLine($"{person} did not get food.");
         }
-        else Console.WriteLine($"{person} did not release lock.");
-      }
-      else Console.WriteLine($"{person} did not get food.");
     }
-  }
 }
